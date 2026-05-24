c314d933e38800615404d68491732ddf32a8e7ed
홍바울 <hongp0508@gmail.com>
2026-05-25 04:08:48 +0900

    perf: Neon fetchConnectionCache 활성화 및 production global DB 캐싱

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,8 @@
-import { neon } from '@neondatabase/serverless'
+import { neon, neonConfig } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from './schema'
+
+neonConfig.fetchConnectionCache = true
 
 declare global {
   // eslint-disable-next-line no-var
@@ -14,9 +16,6 @@ function createDrizzle() {
 }
 
 export function getDb() {
-  if (process.env.NODE_ENV === 'production') {
-    return createDrizzle()
-  }
   if (!global.__db) {
     global.__db = createDrizzle()
   }
